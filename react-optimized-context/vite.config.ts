c17b52cd55d9ac43ea23056ba53117413cd7e1c5
vanthong-iSoft <vanthong@i-soft.com.vn>
2025-03-17 08:52:35 +0700

    change running port

--- react-optimized-context/vite.config.ts
+++ react-optimized-context/vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
-        port: 3333,
+        port: 3301,
     },
 });
--- react-redux/vite.config.ts
+++ react-redux/vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
-        port: 3001,
+        port: 3302,
     },
 });
